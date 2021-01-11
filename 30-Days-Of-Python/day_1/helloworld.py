01fe2dc724e1db55e6ee4e4d3370c6ed7793754a
rohanap <36108401+rohanap@users.noreply.github.com>
2021-01-11 14:51:54 +0530

    Add files via upload

---
+++ 30-Days-Of-Python/day_1/helloworld.py
@@ -0,0 +1,26 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+name = "Harry"
+lname = "Potter"
+country = "Hogwart"
+message = "I am enjoying 30 days of python."
+
+print(name)
+print(lname)
+print(country)
+print(message)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabene','Python','Finland']))
+print(type(name))
+print(type(lname))
+print(type(country))
